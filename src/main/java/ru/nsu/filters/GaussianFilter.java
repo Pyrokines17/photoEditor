d10c7e5c9e4989a8813a56c6e4693231a7d28b6f
IliaBoyaCF <ilia.boya.cf@yandex.ru>
2025-03-31 21:58:15 +0700

    feat: kernel for Gaussian filter is now generating on demand

--- src/main/java/ru/nsu/filters/GaussianFilter.java
+++ src/main/java/ru/nsu/filters/GaussianFilter.java
@@ -4,22 +4,6 @@ import java.awt.image.BufferedImage;
 
 public class GaussianFilter extends MatrixFilter {
 
-    private static final double[] window3 = {
-            1.0 / 16, 2.0 / 16, 1.0 / 16,
-            2.0 / 16, 4.0 / 16, 2.0 / 16,
-            1.0 / 16, 2.0 / 16, 1.0 / 16
-    };
-
-    private static final FilterMatrix matrix3 = new FilterMatrix(window3, 3);
-
-    private static final double[] window5 = {
-        1.0 / 75, 2.0 / 75, 3.0 / 75, 2.0 / 75, 1.0 / 75,
-        2.0 / 75, 4.0 / 75, 5.0 / 75, 4.0 / 75, 2.0 / 75,
-        3.0 / 75, 5.0 / 75, 6.0 / 75, 5.0 / 75, 3.0 / 75,
-        2.0 / 75, 4.0 / 75, 5.0 / 75, 4.0 / 75, 2.0 / 75,
-        1.0 / 75, 2.0 / 75, 3.0 / 75, 2.0 / 75, 1.0 / 75
-    };
-
     public GaussianFilter(Parameters parameters) {
         super(parameters);
     }
@@ -30,10 +14,32 @@ public class GaussianFilter extends MatrixFilter {
     }
 
     private FilterMatrix generate(int size) {
-        return switch (size) {
-            case 3 -> matrix3;
-            case 5 -> new FilterMatrix(window5, 5);
-            default -> throw new IllegalArgumentException("Illegal value(or not implemented).");
-        };
+        if (size % 2 == 0) {
+            throw new IllegalArgumentException("Size must be odd.");
+        }
+
+        double sigma = 1.0;
+        double[] kernel = new double[size * size];
+        int mean = size / 2;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                kernel[j + i * size] = (1 / (2 * Math.PI * sigma * sigma)) * Math.exp(-(Math.pow(i - mean, 2) + Math.pow(j - mean, 2)) / (2 * sigma * sigma));
+            }
+        }
+
+        normalize(kernel, size);
+
+        return new FilterMatrix(kernel, size);
+    }
+
+    private static void normalize(double[] kernel, int size) {
+        double sum = 0;
+        for (int i = 0; i < size * size; ++i) {
+            sum += kernel[i];
+        }
+
+        for (int i = 0; i < size * size; ++i) {
+            kernel[i] = kernel[i] / sum;
+        }
     }
 }
