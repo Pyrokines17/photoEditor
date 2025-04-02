d5e225d0e95ce55e316221e4cf9e8ad0a11d82c1
Timur <gunkotim3@gmail.com>
2025-04-02 23:08:11 +0700

    fixed aqua

--- src/main/java/ru/nsu/filters/Aqua.java
+++ src/main/java/ru/nsu/filters/Aqua.java
@@ -1,9 +1,15 @@
 package ru.nsu.filters;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class Aqua extends Filter {
+    private final static int[] matrix = {
+            0, -1, 0,
+            -1, 5, -1,
+            0, -1, 0
+    };
+
     public Aqua(Parameters parameters) {
         super(parameters);
     }
@@ -11,94 +17,130 @@ public class Aqua extends Filter {
     @Override
     public BufferedImage apply(BufferedImage image, int x, int y) {
         BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
-        newImage.setData(image.getData());
-
-        int width = image.getWidth();
-        int height = image.getHeight();
-
-        Rectangle transform = new Rectangle(0, 0, width, height);
+        int width = image.getWidth(); int height = image.getHeight();
 
         int[] inPixels = getRGB(image, 0, 0, width, height, null);
         inPixels = filterPixels(width, height, inPixels);
-        setRGB(newImage, 0, 0, transform.width, transform.height, inPixels);
+        inPixels = filterPixelsSharpen(width, height, inPixels);
+        setRGB(newImage, 0, 0, width, height, inPixels);
 
         return newImage;
     }
 
-    private int[] filterPixels(int width, int height, int[] inPixels) {
-        int index = 0;
-
-        int levels = 256;
-        int range = 5;
-
-        int[] rHistogram = new int[levels];
-        int[] gHistogram = new int[levels];
-        int[] bHistogram = new int[levels];
-
-        int[] rTotal = new int[levels];
-        int[] gTotal = new int[levels];
-        int[] bTotal = new int[levels];
-
+    private int[] filterPixelsSharpen(int width, int height, int[] inPixels) {
         int[] outPixels = new int[width * height];
+        int index = 0;
 
         for (int y = 0; y < height; ++y) {
             for (int x = 0; x < width; ++x) {
-                for (int i = 0; i < levels; ++i)
-                    rHistogram[i] = gHistogram[i] = bHistogram[i] = rTotal[i] = gTotal[i] = bTotal[i] = 0;
+                outPixels[index++] = applyMatrix(width, height, inPixels, x, y);
+            }
+        }
 
-                for (int row = -range; row <= range; ++row) {
-                    int iy = y+row;
-                    int ioffset;
+        return outPixels;
+    }
+
+    private int applyMatrix(int width, int height, int[] inPixels, int i, int j) {
+        double newR = 0, newG = 0, newB = 0;
+        int rgb, r, g, b;
+
+        for (int mi = -1; mi <= 1; ++mi) {
+            for (int mj = -1; mj <= 1; ++mj) {
+                int x = i + mi; int y = j + mj;
+
+                if (x < 0 || x >= width || y < 0 || y >= height) {
+                    continue;
+                }
+
+                rgb = inPixels[y * width + x];
+
+                r = getRedComponent(rgb);
+                g = getGreenComponent(rgb);
+                b = getBlueComponent(rgb);
+
+                newR += r * matrix[mj + 1 + (mi + 1) * 3];
+                newG += g * matrix[mj + 1 + (mi + 1) * 3];
+                newB += b * matrix[mj + 1 + (mi + 1) * 3];
+            }
+        }
+
+        return convertBack(newR, newG, newB);
+    }
+
+    private int[] filterPixels(int width, int height, int[] inPixels) {
+        int[] outPixels = new int[width * height];
+        int index = 0;
+
+        int[] argb = new int[25];
+        int[] r = new int[25];
+        int[] g = new int[25];
+        int[] b = new int[25];
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                int k = 0;
+
+                for (int dy = -2; dy <= 2; ++dy) {
+                    int iy = y + dy;
 
                     if (0 <= iy && iy < height) {
-                        ioffset = iy*width;
+                        int ioffset = iy * width;
 
-                        for (int col = -range; col <= range; ++col) {
-                            int ix = x+col;
+                        for (int dx = -2; dx <= 2; ++dx) {
+                            int ix = x + dx;
 
                             if (0 <= ix && ix < width) {
-                                int rgb = inPixels[ioffset+ix];
+                                int rgb = inPixels[ioffset + ix];
 
-                                int r = (rgb >> 16) & 0xff;
-                                int g = (rgb >> 8) & 0xff;
-                                int b = rgb & 0xff;
-
-                                int ri = r*levels/256;
-                                int gi = g*levels/256;
-                                int bi = b*levels/256;
-
-                                rTotal[ri] += r;
-                                gTotal[gi] += g;
-                                bTotal[bi] += b;
-
-                                rHistogram[ri]++;
-                                gHistogram[gi]++;
-                                bHistogram[bi]++;
+                                argb[k] = rgb;
+                                r[k] = (rgb >> 16) & 0xff;
+                                g[k] = (rgb >> 8) & 0xff;
+                                b[k] = rgb & 0xff;
+                                k++;
                             }
                         }
                     }
                 }
 
-                int r = 0, g = 0, b = 0;
-
-                for (int i = 1; i < levels; ++i) {
-                    if (rHistogram[i] > rHistogram[r])
-                        r = i;
-                    if (gHistogram[i] > gHistogram[g])
-                        g = i;
-                    if (bHistogram[i] > bHistogram[b])
-                        b = i;
+                while (k < 25) {
+                    argb[k] = 0xff000000;
+                    r[k] = g[k] = b[k] = 0;
+                    k++;
                 }
 
-                r = rTotal[r] / rHistogram[r];
-                g = gTotal[g] / gHistogram[g];
-                b = bTotal[b] / bHistogram[b];
+                outPixels[index++] = argb[rgbMedian(r, g, b)];
 
-                outPixels[index] = (inPixels[index] & 0xff000000) | ( r << 16 ) | ( g << 8 ) | b;
-                index++;
+                /*
+                Arrays.sort(r);
+                Arrays.sort(g);
+                Arrays.sort(b);
+
+                outPixels[index++] = convertBack(r[12], g[12], b[12]);
+                */
             }
         }
 
         return outPixels;
+    }
+
+    private int rgbMedian(int[] r, int[] g, int[] b) {
+        int sum, index = 0, min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 25; ++i) {
+            sum = 0;
+
+            for (int j = 0; j < 25; ++j) {
+                sum += Math.abs(r[i]-r[j]);
+                sum += Math.abs(g[i]-g[j]);
+                sum += Math.abs(b[i]-b[j]);
+            }
+
+            if (sum < min) {
+                min = sum;
+                index = i;
+            }
+        }
+
+        return index;
     }
 }
